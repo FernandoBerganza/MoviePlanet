12e4a5d99e42f29dfa2bbea23467a22cebb2662a
luisjo01 <144155171+luisjo01@users.noreply.github.com>
2024-10-17 17:28:04 -0600

    16-10 2 se intenta agregar la conexion a la base de datos
    tambien se modifica el build gradle app

--- MyApplication - copia/app/src/main/java/com/example/myapplication/MainGestor.java
+++ MyApplication - copia/app/src/main/java/com/example/myapplication/MainGestor.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -70,7 +71,6 @@ public class MainGestor extends AppCompatActivity {
     }
 
     private void cerrarBoleto() {
-        // Lógica para cerrar un boleto
     }
 }
 
